c0c4abe574b86734316a411ac7294537ced4c2c2
maksimkla19 <68090252+maksimkla19@users.noreply.github.com>
2023-11-18 12:37:21 +0600

    Add files via upload

--- main.rb
+++ main.rb
@@ -5,7 +5,7 @@ require_relative 'station'
 require_relative 'route'
 require_relative 'wagon'
 require_relative 'wagon_cargo'
-require_relative 'wagon_passanger'
+require_relative 'wagon_passenger'
 require_relative 'menus'
 
 Menus.new
